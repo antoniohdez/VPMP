3e1258f1cb8d479580ffdb61c0a8ca2e900c505b
Antonio <antonio.hdez93@gmail.com>
2015-03-28 13:45:07 -0600

    Partial views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.template import Context, Template
 
 # Create your views here.
 
@@ -10,16 +11,25 @@ def template(request):
     return HttpResponse(template.render(context))
 
 def index(request):
-    template = loader.get_template("views/index.html")
-    context = RequestContext(request, {})
+    template = loader.get_template("general/template.html")
+    html = loader.render_to_string("views/index.html", {})
+    context = RequestContext(request, {
+        'content': htmlg
+        })
     return HttpResponse(template.render(context))
 
 def report(request):
-    template = loader.get_template("views/report-detail.html")
-    context = RequestContext(request, {})
+    template = loader.get_template("general/template.html")
+    html = loader.render_to_string("views/report-detail.html", {})
+    context = RequestContext(request, {
+        'content': html
+        })
     return HttpResponse(template.render(context))
 
 def generate(request):
-    template = loader.get_template("views/report-create.html")
-    context = RequestContext(request, {})
+    template = loader.get_template("general/template.html")
+    html = loader.render_to_string("views/report-create.html", {})
+    context = RequestContext(request, {
+        'content': html
+        })
     return HttpResponse(template.render(context))
