f5693e242cfa2a4cc7bb1be74d0a0c3700f0416f
Antonio <antonio.hdez93@gmail.com>
2015-05-09 13:47:24 -0500

    django-cors-headers

--- django_project/settings.py
+++ django_project/settings.py
@@ -36,11 +36,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'app',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -89,3 +91,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = '/media/'
+
+CORS_ORIGIN_ALLOW_ALL = True
